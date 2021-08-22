04bbf3543f61bcee8a33b8322334c7fac8b5c300
Seunghwan <ysp1307@naver.com>
2021-08-23 00:01:12 +0900

    로딩 화면 스타일링 재구성

--- pages/LoadingPage.js
+++ pages/LoadingPage.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 import { t } from "react-native-tailwindcss";
+import { AntDesign } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function LoadingPage() {
   return (
-    <View style={[t.bgYellow300, t.flex1, t.justifyCenter, t.itemsCenter]}>
+    <LinearGradient
+      // Button Linear Gradient
+      colors={["#feac5e", "#c779d0", "#4bc0c8"]}
+      style={[t.bgYellow300, t.flex1, t.justifyCenter, t.itemsCenter]}
+    >
+      <StatusBar barStyle="dark-content" />
+
       <View
         style={[
-          t.bgWhite,
+          t.bgTransparent,
           t.w11_12,
           t.shadow2xl,
           t.hAuto,
@@ -15,8 +23,10 @@ export default function LoadingPage() {
           t.itemsCenter,
         ]}
       >
-        <Text style={[t.textXl, t.pB22, t.p20]}>Getting the weather </Text>
+        <AntDesign name="hourglass" size={54} color="black" style={[t.pT10]} />
+        <Text style={[t.text4xl, t.pT5]}>WeatherCheck </Text>
+        <Text style={[t.textXs, t.pB5, t.p2]}>Now Loading... </Text>
       </View>
-    </View>
+    </LinearGradient>
   );
 }
